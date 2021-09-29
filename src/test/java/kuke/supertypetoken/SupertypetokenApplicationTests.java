f6688b3a39f7fbd8bb5cbeb5f008304bcd7dbfd8
SongHeeJae <gmlwo308@gmail.com>
2021-09-29 23:40:54 +0900

    first commit

---
+++ src/test/java/kuke/supertypetoken/SupertypetokenApplicationTests.java
@@ -0,0 +1,13 @@
+package kuke.supertypetoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupertypetokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
