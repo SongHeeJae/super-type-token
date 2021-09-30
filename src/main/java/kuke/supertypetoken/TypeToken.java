6e68dec4761795d388c3c1a9586a822c24b1cbe8
SongHeeJae <gmlwo308@gmail.com>
2021-09-30 12:27:41 +0900

    type token의 한계

--- src/main/java/kuke/supertypetoken/TypeToken.java
+++ src/main/java/kuke/supertypetoken/TypeToken.java
@@ -24,8 +24,12 @@ public class TypeToken {
         final TypesafeMap m = new TypesafeMap();
         m.put(String.class, "Value");
         m.put(Integer.class, 3);
-        m.put(List.class, Arrays.asList(1, 2, 3));
-        
+
+        // type token의 한계 아래 두 개를 구분하지 못함
+        // 제너릭으로 타입 파라미터를 지정할 수도 없음
+//        m.put(List<Integer>.class, Arrays.asList(1, 2, 3));
+        m.put(List.class, Arrays.asList("a", "b", "c"));
+
         System.out.println("m.get(Integer.class) = " + m.get(Integer.class));
         System.out.println("m.get(String.class) = " + m.get(String.class));
 
