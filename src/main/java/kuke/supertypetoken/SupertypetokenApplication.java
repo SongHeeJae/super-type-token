31d628b2ef12feb7f9a5cab2eb4f35d763224b56
SongHeeJae <gmlwo308@gmail.com>
2021-09-30 13:45:20 +0900

    RestTemplate으로 super type token(ParameterizedTypeReference) 적용

--- src/main/java/kuke/supertypetoken/SupertypetokenApplication.java
+++ src/main/java/kuke/supertypetoken/SupertypetokenApplication.java
@@ -2,6 +2,11 @@ package kuke.supertypetoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SupertypetokenApplication {
@@ -10,4 +15,36 @@ public class SupertypetokenApplication {
 		SpringApplication.run(SupertypetokenApplication.class, args);
 	}
 
+
+	@RestController
+	public static class MyController {
+
+		@RequestMapping("/")
+		public List<User> users() {
+			return Arrays.asList(new User("a"),
+					new User("b"), new User("c"));
+		}
+	}
+
+	public static class User {
+		String name;
+
+		public User() {
+		}
+
+		public User(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public String toString() {
+			return "User{" +
+					"name='" + name + '\'' +
+					'}';
+		}
+	}
 }
